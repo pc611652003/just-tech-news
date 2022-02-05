5c204f0e59c50dc0d413c92cbf666a8d98234a5d
pc611652003 <88979797+pc611652003@users.noreply.github.com>
2022-02-05 09:45:34 -0700

    added partial for display

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
